0a647e6f6a2fbb5313a0070b7a1e09fe21058674
Benjamin <benjamin.petelinek@gmail.com>
2024-06-01 16:28:39 +0200

    meni

---
+++ notebooks/Projekt/grajenje_modela2.py
@@ -0,0 +1,179 @@
+import joblib
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import f_regression
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from tensorflow.keras.layers import Input, LSTM, Dense, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
+import mlflow
+from sklearn.base import TransformerMixin
+import os
+from sklearn.preprocessing import FunctionTransformer
+
+# Nastavitev sledenja MLflow
+mlflow.set_tracking_uri('https://dagshub.com/PetelinekBenjamin/projekt3.mlflow')
+
+# Nastavitev uporabniškega imena in gesla
+os.environ["MLFLOW_TRACKING_USERNAME"] = "PetelinekBenjamin"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "30663408e580bdb3f66e074627577a040f36b5ff"
+
+def pripravi_podatke_za_ucenje(vrednosti, okno_velikost):
+    X, y = [], []
+    for i in range(len(vrednosti) - okno_velikost):
+        X.append(vrednosti[i:i+okno_velikost, :])
+        y.append(vrednosti[i+okno_velikost, -1])
+    return np.array(X), np.array(y)
+
+def fill_missing_values(X):
+    imputer = SimpleImputer(strategy='mean')
+    X_imputed = imputer.fit_transform(X)
+    return X_imputed
+
+
+
+# Začetek MLflow teka
+with mlflow.start_run(run_name="MyModelTraining2Ucenje"):
+    # Preberi podatke
+    pot_do_datoteke = r'C:\Users\benja\Desktop\JOKE\Projekt-master\Projekt-master\data\processed\train_prod.csv'
+    df = pd.read_csv(pot_do_datoteke, parse_dates=['Cas'], index_col='Cas')
+
+    # Izloči manjkajoče vrednosti
+    print(df.isnull().sum())
+
+    # Sortiranje po času
+    df.sort_index(inplace=True)
+
+    # Dodajanje stolpcev day, month in year
+    datum = pd.to_datetime(df.index, format='%d/%m/%Y')
+    df['day'] = datum.day
+    df['month'] = datum.month
+    df['year'] = datum.year
+
+    print(df.columns)
+    print(df.tail())
+
+    # Filtriranje značilnic
+    najdoprinosne_znacilnice = [
+    'day',
+    'month',
+    'year',
+    'Temperatura (2m)',
+    'Relativna vlaga (2m)',
+    'Temperatura rosisca (2m)',
+    'Obcutna temperatura',
+    'Verjetnost padavin',
+    'Stevilo nesrec',
+    'Free flow speed',
+    'Current travel time',
+    'Free flow travel time',
+    'Confidence',
+    
+]
+
+    ciljna_znacilnica = 'Average speed'
+    podatki = df[najdoprinosne_znacilnice + [ciljna_znacilnica]]
+
+    # Definicija cevovoda za predprocesiranje podatkov
+    preprocessing_pipeline = ColumnTransformer([
+        ('fill_missing', FunctionTransformer(fill_missing_values), ['Temperatura (2m)',
+    'Relativna vlaga (2m)',
+    'Temperatura rosisca (2m)',
+    'Obcutna temperatura',
+    'Verjetnost padavin',
+    'Stevilo nesrec',
+    'Free flow speed',
+    'Current travel time',
+    'Free flow travel time',
+    'Confidence',
+    ]),
+        ('scaler', StandardScaler(), ['day',
+    'month',
+    'year',
+    'Temperatura (2m)',
+    'Relativna vlaga (2m)',
+    'Temperatura rosisca (2m)',
+    'Obcutna temperatura',
+    'Verjetnost padavin',
+    'Stevilo nesrec',
+    'Free flow speed',
+    'Current travel time',
+    'Free flow travel time',
+    'Confidence',]),
+        ('scaler1', StandardScaler(), ['Average speed']),
+    ])
+
+
+
+
+# Uporaba cevovoda za predprocesiranje podatkov
+    processed_data = preprocessing_pipeline.fit_transform(podatki)
+
+    print(processed_data.shape)
+
+    train_data = processed_data
+
+    # Pridobitev skalirnika iz cevovoda za predprocesiranje podatkov
+    scaler = preprocessing_pipeline.named_transformers_['scaler']
+
+    # Shranjevanje scalera v datoteko
+    scaler_path = r"C:\Users\benja\Desktop\JOKE\Projekt-master\Projekt-master\models\model2_scaler_pipeline1.pkl"
+    joblib.dump(scaler, scaler_path)
+
+
+
+
+
+    # Pridobitev skalirnika iz cevovoda za predprocesiranje podatkov
+    scaler1 = preprocessing_pipeline.named_transformers_['scaler1']
+
+    # Shranjevanje scalera v datoteko
+    scaler_path1 = r"C:\Users\benja\Desktop\JOKE\Projekt-master\Projekt-master\models\model2_scaler_pipeline2.pkl"
+    joblib.dump(scaler1, scaler_path1)
+
+    print("Oblika učnih podatkov:", train_data.shape)
+
+    
+
+
+    # Priprava podatkov za model
+    okno_velikost = 2
+    X_train, y_train = pripravi_podatke_za_ucenje(train_data, okno_velikost)
+    y_train = y_train.flatten()
+
+    # Definicija modela
+    inputs = Input(shape=(X_train.shape[1], X_train.shape[2]))
+    lstm1 = LSTM(256, return_sequences=True)(inputs)
+    lstm2 = LSTM(256)(lstm1)
+    dense1 = Dense(64, activation='relu')(lstm2)
+    dropout = Dropout(0.2)(dense1)
+    outputs = Dense(1)(dropout)
+    model_lstm = Model(inputs=inputs, outputs=outputs)
+
+    # Kompilacija modela
+    model_lstm.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'mse'])
+
+    # Early Stopping
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+    # Učenje modela
+    history = model_lstm.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.1, callbacks=[early_stopping], verbose=1)
+
+    # Beleženje parametrov
+    mlflow.log_param("epochs", 10)
+    mlflow.log_param("batch_size", 32)
+    mlflow.log_param("validation_split", 0.1)
+    mlflow.log_param("patience", 3)
+
+    # Beleženje modela
+    mlflow.keras.log_model(model_lstm, "model")
+
+    # Shranjevanje modela
+    model_path = r"C:\Users\benja\Desktop\JOKE\Projekt-master\Projekt-master\models\model2_model_lstm.pkl"
+    joblib.dump(model_lstm, model_path, compress=True)
---
+++ notebooks/Projekt/ovrednotenje_modela2.py
@@ -0,0 +1,228 @@
+import joblib
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import f_regression
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.layers import Input, LSTM, Dense, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
+from sklearn.compose import ColumnTransformer
+import mlflow
+import os
+from tensorflow.keras.models import load_model
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import FunctionTransformer
+import mlflow.pyfunc
+ 
+
+
+
+
+
+
+
+# Nastavitev sledenja MLflow
+mlflow.set_tracking_uri('https://dagshub.com/PetelinekBenjamin/projekt3.mlflow')
+# mlflow.set_tracking_uri("https://dagshub.com/ZanPovseGit/inteligentniSistem.mlflow")
+
+# Nastavitev uporabniškega imena in gesla
+os.environ["MLFLOW_TRACKING_USERNAME"] = "PetelinekBenjamin"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "30663408e580bdb3f66e074627577a040f36b5ff"
+# os.environ["MLFLOW_TRACKING_USERNAME"] = "ZanPovseGit"
+# os.environ["MLFLOW_TRACKING_PASSWORD"] = "bdf091cc3f58df2c8346bb8ce616545e0e40b351"
+
+def pripravi_podatke_za_ucenje(vrednosti, okno_velikost):
+    X, y = [], []
+    for i in range(len(vrednosti) - okno_velikost):
+        X.append(vrednosti[i:i+okno_velikost, :])
+        y.append(vrednosti[i+okno_velikost, -1])
+    return np.array(X), np.array(y)
+
+def fill_missing_values(X):
+    imputer = SimpleImputer(strategy='mean')
+    X_imputed = imputer.fit_transform(X)
+    return X_imputed
+
+# Začetek MLflow teka
+with mlflow.start_run(run_name="OvrednotenjeModela2"):
+
+
+    # Preberi podatke
+
+    pot_do_datoteke = r'C:\Users\benja\Desktop\JOKE\Projekt-master\Projekt-master\data\processed\test_prod.csv'
+    df = pd.read_csv(pot_do_datoteke, parse_dates=['Cas'], index_col='Cas')
+
+
+    # Izloči manjkajoče vrednosti
+    print(df.isnull().sum())
+
+    # Sortiranje po času
+    df.sort_index(inplace=True)
+
+    # Dodajanje stolpcev day, month in year
+    datum = pd.to_datetime(df.index, format='%d/%m/%Y')
+    df['day'] = datum.day
+    df['month'] = datum.month
+    df['year'] = datum.year
+
+    print(df.columns)
+    print(df.tail())
+
+    # Filtriranje značilnic
+    # Filtriranje značilnic
+    najdoprinosne_znacilnice = [
+    'day',
+    'month',
+    'year',
+    'Temperatura (2m)',
+    'Relativna vlaga (2m)',
+    'Temperatura rosisca (2m)',
+    'Obcutna temperatura',
+    'Verjetnost padavin',
+    'Stevilo nesrec',
+    'Free flow speed',
+    'Current travel time',
+    'Free flow travel time',
+    'Confidence',
+    
+]
+
+    ciljna_znacilnica = 'Average speed'
+    podatki = df[najdoprinosne_znacilnice + [ciljna_znacilnica]]
+
+    # Load the scaler from the file
+    scaler_path = r"C:\Users\benja\Desktop\JOKE\Projekt-master\Projekt-master\models\model2_scaler_pipeline1.pkl"
+    scaler = joblib.load(scaler_path)
+
+    scaler_path1 = r"C:\Users\benja\Desktop\JOKE\Projekt-master\Projekt-master\models\model2_scaler_pipeline2.pkl"
+    scaler1 = joblib.load(scaler_path1)
+
+    # Definicija cevovoda za predprocesiranje podatkov
+    preprocessing_pipeline = ColumnTransformer([
+        ('fill_missing', FunctionTransformer(fill_missing_values), ['Temperatura (2m)',
+    'Relativna vlaga (2m)',
+    'Temperatura rosisca (2m)',
+    'Obcutna temperatura',
+    'Verjetnost padavin',
+    'Stevilo nesrec',
+    'Free flow speed',
+    'Current travel time',
+    'Free flow travel time',
+    'Confidence',
+    ]),
+        ('scaler', StandardScaler(), ['day',
+    'month',
+    'year',
+    'Temperatura (2m)',
+    'Relativna vlaga (2m)',
+    'Temperatura rosisca (2m)',
+    'Obcutna temperatura',
+    'Verjetnost padavin',
+    'Stevilo nesrec',
+    'Free flow speed',
+    'Current travel time',
+    'Free flow travel time',
+    'Confidence',]),
+        ('scaler1', StandardScaler(), ['Average speed']),
+    ])
+
+    processed_data = preprocessing_pipeline.fit_transform(podatki)
+
+
+
+    test_data = processed_data
+    print("Oblika učnih podatkov:", test_data.shape)
+
+
+    # Priprava podatkov za model
+    okno_velikost = 2
+    X_test, y_test = pripravi_podatke_za_ucenje(test_data, okno_velikost)
+    y_test = y_test.flatten()
+
+
+
+    model_lstm = joblib.load(r"C:\Users\benja\Desktop\JOKE\Projekt-master\Projekt-master\models\model2_model_lstm.pkl")
+    
+    
+
+
+    
+
+
+
+    
+
+
+
+    # Preverjanje uspešnosti modela
+    y_pred = model_lstm.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    # Beleženje metrik
+    mlflow.log_metrics({"mse": mse, "mae": mae, "r2": r2})
+
+    # Shranjevanje modela v MLflow
+    mlflow.keras.log_model(model_lstm, "lstm_model")
+
+    # Prehod trenutnega modela v fazo produkcije
+    #mlflow.set_tag("tag", "production2")
+    #mlflow.register_model("runs:/" + mlflow.active_run().info.run_id + "/lstm_model", "OvrednotenjeModela2")
+
+
+    print("MSE:", mse)
+    print("MAE:", mae)
+    print("R2:", r2)
+
+    # Pridobite informacije o zadnjem zagonu z oznako "production"
+    last_run = mlflow.search_runs(filter_string="tags.tag='production2'",
+                              order_by=["start_time DESC"],
+                              max_results=1).iloc[0]
+
+    # Pridobite ID zadnjega zagona
+    last_run_id = last_run["run_id"]
+    print("ID zadnjega zagona s tagom 'production':", last_run_id)
+
+
+    run_info = mlflow.get_run(last_run_id)
+    # Pridobitev metrik
+    metrics_production = run_info.data.metrics
+
+    # Pridobitev modela
+    model_uri = "runs:/" + last_run_id + "/lstm_model"
+    model = mlflow.pyfunc.load_model(model_uri)
+
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    print("DELA",mse)
+    print("DELA",mae)
+    print("DELA",r2)
+
+
+    print(metrics_production)
+
+    mse_prod = metrics_production["mse"]
+    mae_prod = metrics_production["mae"]
+    r2_prod = metrics_production["r2"]
+
+    print("mse_prod", mse_prod)
+    print("mae_prod", mae_prod)
+    print("r2_prod", r2_prod)
+
+    # Izračun povprečja metrik za testno in produkcijsko okolje
+    avg_test = (mse + mae + r2) / 3
+    avg_prod = (mse_prod + mae_prod + r2_prod) / 3
+
+    if mse < mse_prod:
+        mlflow.set_tag("tag", "production2")
+        mlflow.register_model("runs:/" + mlflow.active_run().info.run_id + "/lstm_model", "Production2")
+    else:
+        mlflow.set_tag("tag", "test2")
+        mlflow.register_model("runs:/" + mlflow.active_run().info.run_id + "/lstm_model", "Test2")
+        
